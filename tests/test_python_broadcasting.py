756dbbacfdf89724334ab46122c63a45d1359c68
jrogulski <jakubrogulski01@gmail.com>
2024-05-30 09:19:51 +0200

    fixed styling

--- tests/test_python_broadcasting.py
+++ tests/test_python_broadcasting.py
@@ -2,16 +2,22 @@ import unittest
 import torch
 from tensor_broadcasting import broadcast_tensors
 
-class TestCalculations(unittest.TestCase):
-    
-    def setUp(self):
-        self.tensor1 = torch.tensor([[1,2,3], [4,5,6]])
-        self.tensor2 = torch.tensor([1,1,1])
-        self.broadcasted_tensor2_shape, self.broadcasted_tensor2 = broadcast_tensors(self.tensor1, self.tensor2)
-        
-    def test_broadcasted_shape(self):
-    
-        self.assertEqual(self.broadcasted_tensor2_shape, (2, 3), 'Shape mismatch')
 
-if __name__ == '__main__':
+class TestCalculations(unittest.TestCase):
+
+    def setUp(self):
+        self.tensor1 = torch.tensor([[1, 2, 3], [4, 5, 6]])
+        self.tensor2 = torch.tensor([1, 1, 1])
+        self.broadcasted_tensor2_shape, self.broadcasted_tensor2 = (
+            broadcast_tensors(self.tensor1, self.tensor2)
+        )
+
+    def test_broadcasted_shape(self):
+
+        self.assertEqual(
+            self.broadcasted_tensor2_shape, (2, 3), "Shape mismatch"
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
