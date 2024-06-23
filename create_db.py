43df23beefd5d871c9f808b87c942361b5c161d9
jakub <jakubrogulski01@gmail.com>
2024-06-23 15:39:45 +0200

    add test table

--- create_db.py
+++ create_db.py
@@ -14,15 +14,44 @@ def create_database():
 
     cur = conn.cursor()
 
-    cur.execute("SELECT 1 FROM pg_database WHERE datname=postgres")
+    cur.execute("SELECT 1 FROM pg_database WHERE datname='postgres'")
     exists = cur.fetchone()
     if not exists:
         cur.execute('CREATE DATABASE postgres')
     else:
-        print("Database 'yourdbname' already exists")
+        print("Database 'postgres' already exists")
 
     cur.close()
     conn.close()
     print("Database created")
+
+def create_table_and_insert_rows():
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        password='passwrd',
+        host='db',  
+        port='5432'
+    )
+
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS test (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100),
+            age INT
+        )
+    """)
+
+    cur.execute("INSERT INTO test (name, age) VALUES (%s, %s)", ("Alice", 30))
+    cur.execute("INSERT INTO test (name, age) VALUES (%s, %s)", ("Bob", 25))
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
+    print("Table created and rows inserted")
+
 if __name__ == '__main__':
     create_database()
+    create_table_and_insert_rows()
